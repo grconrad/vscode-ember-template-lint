76465b73060ed8df6ab54292b93b31933e69b821
Richard Conrad <rconrad@linkedin.com>
2020-05-10 23:11:53 -0700

    Again

--- scripts/pretest.sh
+++ scripts/pretest.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
-
 # Install dependencies in sample project used by tests.
 
-# This could also be done in the test itself, but it's a bug uglier to run a yarn install from JS
+# This could also be done in the test itself, but it's uglier to run a yarn install from JS
 # and adjust timeouts and so on.
 echo "Installing dependencies in sample project"
-cd test-fixtures/sample-project
+cd ./test-fixtures/sample-project
 yarn install
 
 cd ../..
